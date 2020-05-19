3a076df607bdb905f5d6c04c74dd94bea271b74b
Makkpas <axisfs12@gmail.com>
2020-05-18 22:34:41 -0600

    Pruebas de commit remoto

--- resources/views/libros/index.blade.php
+++ resources/views/libros/index.blade.php
@@ -4,7 +4,8 @@
 
     <div class="container">
         {{-- Para traducir estan 2 formas--}}
-        {{-- <h2>{{__('Acá van a ir todo los libros')}}</h2> --}}
+        {{-- <h2>{{__('Acá van a ir todo los libros')}}</h2> --}} 
+        
         <h2>@lang('Acá van a ir todo los libros')</h2>
     </div>
         
