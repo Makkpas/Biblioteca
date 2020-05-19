cb5a6110add5886a3c2273132388fd299993d9db
Makkpas <axisfs12@gmail.com>
2020-05-18 22:45:33 -0600

    Cambios en index.blade para probar commit remoto

--- resources/views/libros/index.blade.php
+++ resources/views/libros/index.blade.php
@@ -6,7 +6,6 @@
         {{-- Para traducir estan 2 formas--}}
         {{-- <h2>{{__('Acá van a ir todo los libros')}}</h2> --}} 
         
-        
         <h2>@lang('Acá van a ir todo los libros')</h2>
     </div>
         
